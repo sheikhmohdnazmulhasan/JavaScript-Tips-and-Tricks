be1769fb9e931c947d1e514a5030924040b0602e
mdnazmul42726 <nazmulofficial@outlook.com>
2023-12-28 02:16:26 +0600

    Problem-1 Remove Fallacy Values and Array

---
+++ Part-1/problem-1.js
@@ -0,0 +1,12 @@
+// Remove Fallacy Values and Array
+
+// We know there are six false values in JavaScript.
+//  1.false, 2.undefined, 3.null, 4.NaN, 5.0, 6.''
+
+// We have an array and from this array we will remove all the fallacy values and extract the only truthy values.
+const miscellaneous = ['🍎', false, '🍌', undefined, '🥑', NaN, ' 🫑', null, '🥦', ''];
+
+// passing Boolean to array.filter() will remove falsy value from array
+const fruits = miscellaneous.filter(Boolean);
+
+console.log(fruits); // [ '🍎', '🍌', '🥑', ' 🫑', '🥦' ]
