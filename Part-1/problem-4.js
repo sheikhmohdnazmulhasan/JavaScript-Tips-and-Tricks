bb9335487a99dff2617b5d2890271b7af8eda982
mdnazmul42726 <nazmulofficial@outlook.com>
2023-12-28 03:10:55 +0600

    flatten a multi-dimensional array

---
+++ Part-1/problem-4.js
@@ -0,0 +1,14 @@
+//  How to flatten a multi-dimensional array
+
+const smileys = ['😃', '😀', '😄', ['😁', '😆', '😌'], '🤩', ['😉', '🙃', '😂'], ['🥹', '😅']];
+
+// We use array.flat() method to flatten one-level array
+const flatten = smileys.flat();
+console.log(flatten); //['😃', '😀', '😄', '😁', '😆', '😌', '🤩', '😉', '🙃', '😂', '🥹' , '😅']
+
+// Multi-level array
+const smileys2 = ['😃', '😀', '😄', ['😁', ['😆'], '😌'], '🤩', ['😂', ['😉', ['🙃']]], ['🥹', ['😅']]];
+
+// We use array.flat(Infinity) method to flatten multi-level array
+const flatten2 = smileys.flat(Infinity);
+console.log(flatten2); //['😃', '😀', '😄', '😁', '😆', '😌', '🤩', '😉', '🙃', '😂', '🥹' , '😅']
