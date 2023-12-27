327e03fd1c599fcaebbb821436dc01329da98940
mdnazmul42726 <nazmulofficial@outlook.com>
2023-12-28 02:51:01 +0600

    Resizing an array

---
+++ Part-1/problem-3.js
@@ -0,0 +1,10 @@
+// Resizing an array 
+
+const animals = [ '🫏', '🦄', '🐍', '🦖', '🦕', '🐘', '🦛', '🦏'];
+
+console.log(animals); // [ '🫏', '🦄', '🐍', '🦖', '🦕', '🐘', '🦛', '🦏'];
+
+//We can use array's length property
+animals.length = 5
+
+console.log(animals); // [ '🫏' , '🦄', '🐍', '🦖', '🦕' ]
